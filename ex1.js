439b372356281073ae72f036758c65fe008cd924
BEATRIZ APARECIDA DA SILVA <58901049856@info.local>
2024-05-09 08:43:12 -0300

    feat: exercio 01

---
+++ ex1.js
@@ -0,0 +1,31 @@
+//Exercício 1: Verificação de login
+//Escreva um programa que solicita ao usuário um nome de usuário 
+//e uma senha. Verifique se o nome de usuário é
+// "ädmin" e a senha é "senha123". 
+//Exiba uma mensagem indicando se o login foi bem-sucedido ou não.
+
+//solocita ao usuario um nome de usuario
+let username = prompt("Digite um nome de usuario: ");
+
+//solicita ao usuario uma senha
+let senha = prompt("Digite a sua senha: ");
+
+//verifique se nome de usuario é "admin" e a senha é "senha123".
+//CASO SIM - exiba a mernsagem de login bem sucedida.
+//CASO NAO - exiba mensagen de usuario ou senha incorreta.
+
+if (username == "admin" && senha == "senha123"); 
+    console.log("Login bem sucedido");
+
+else {
+    console.log("Você não tem permissao de acesso")
+}
+
+//Exiba uma mensagem indicando se o login foi bem-sucedido ou nao.
+//apresentar alhuma mensagem na tela
+
+alert("Hello Word!")
+
+console.log("!Console: Hello Word!")
+
+ptompt("Mensagem na tela que pega alguma informacao")
