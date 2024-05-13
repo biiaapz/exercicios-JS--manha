db20cd544e3635583e726614251e6d7d0f1307c8
BEATRIZ APARECIDA DA SILVA <58901049856@info.local>
2024-05-13 08:27:56 -0300

    feat:exercio 7

--- ex7.js
+++ ex7.js
@@ -8,35 +8,22 @@
 //Valor total da compra acima de R$ 200,00: desconto de 20%.
 //Exiba o valor final da compra com o desconto aplicado.
 
-let valorCompra = prompt("digite o valor total da compra:")
+let valorCompra = parseFloat(prompt("informe o valor total da compra: "))
 
-//converte o valor da compra para um numero
+let desconto;
 
- valorCompra = parseFloat(valorCompra);
 
-//define variavel para aramazenar o valor final da compra com desconto
-
-let valorFinal;
-
-// verifiaca o valor da compra e aplica o desconto progressivo
-
-if (valorCompra <= 100) {
-
-    //sem desconto
-
-    valorFinal = valorCompra;
-} else if (valorCompra <= 200) {
-
-    //desconto de 10%
-
-    valorFinal = valorCompra * 0.9;
+if (valorTotal <= 100) {
+    desconto = 0;
+    console.log("o valor da compra foi: " + desconto);
+} else if (valorTotal <= 200) {
+    desconto = 0.1;
+    console.log("o valor de desconto foi: " + (desconto*100)+ "%");
 } else {
-    
-    //desconto 20%
-
-    valorFinal = valorCompra * 0.8;
+    desconto = 0.2;
+    console.log("o valor de desconto foi: " + (desconto*100)+ "%");
 }
 
-//exibe o valor da compra com desconto aplicado
+let valorFinal = valorTotal - (valorTotal*desconto);
 
-console.log("o valor final da compra com desconto e: R$" + valorFinal.toFixed(2));
+console.log ("o valor final da compra com desconto: R$" + valorFinal.toFixed(2));
